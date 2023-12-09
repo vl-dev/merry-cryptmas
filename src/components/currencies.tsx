4abb857e472aae7b37aea8fa1ba2481c869efe96
Vladislav Matus <vladislav@matus.pro>
2023-12-09 23:36:28 +0100

    token account initialization + deposit

--- src/components/currencies.tsx
+++ src/components/currencies.tsx
@@ -55,7 +55,7 @@ const Currencies: React.FC<props> = (props) => {
         }
       </div>
       <div className="text-gray-500 text-center w-full flex flex-col gap-2 justify-center items-center rounded-xl">
-        <div>Other currencies than SOL add a small fee per recipient (~0.0022 SOL),</div>
+        <div>Other currencies than SOL add a small fee per recipient (~0.0031 SOL),</div>
         <div>to create a token account and make the tokens claimable.</div>
         <div>This SOL will be claimable by the recipient as well.</div>
       </div>
